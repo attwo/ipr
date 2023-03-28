3542340e612ffd661369989477810b5893bea58a
attwo <anntakefour@gmail.com>
2023-03-28 16:11:58 +0300

    test(routes): Добавлен тест для эндпоинта ipr/calc

--- src/tests/test_routes.py
+++ src/tests/test_routes.py
@@ -4,8 +4,19 @@ import pytest
 
 @pytest.fixture
 def generate_data_for_tests():
-    pass
+    return {
+        "p_res": np.random.uniform(200, 300),
+        "wct": np.random.uniform(0, 100),
+        "pi": np.random.uniform(0.5, 3),
+        "pb": np.random.uniform(50, 300),
+    }
 
 
 def test_calc_model_success(api_client, generate_data_for_tests):
-    pass
+    response = api_client.post("ipr/calc",
+                              json=generate_data_for_tests)
+    assert response.status_code == 200
+    result = response.json()
+    assert result
+    assert result["q_liq"]
+    assert result["p_wf"]
