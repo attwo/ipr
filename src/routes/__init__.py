30c057fa0a6386bb0d53541c2f6a0ee9e5f22a1d
attwo <anntakefour@gmail.com>
2023-03-28 16:10:17 +0300

    refactor(routes): Переименована функция my_profile
    Функция my_profile, обрабатывающая эндпоинт /ipt/calc, переименована в calc_ipr

--- src/routes/__init__.py
+++ src/routes/__init__.py
@@ -5,7 +5,7 @@ main_router = APIRouter(prefix="/ipr", tags=["IPR"])
 
 
 @main_router.post("/calc", response_model=IprCalcResponse)
-async def my_profile(ipr_in: IprCalcRequest):
+async def calc_ipr(ipr_in: IprCalcRequest):
     """Эндпоинт расчёта IPR"""
     from src.calculations.vogel_ipr import calc_ipr
     res = calc_ipr(p_res=ipr_in.p_res, pi=ipr_in.pi, wct=ipr_in.wct, pb=ipr_in.pb)
